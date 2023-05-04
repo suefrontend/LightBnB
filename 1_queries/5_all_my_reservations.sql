e1c083401cb325aeb36693557eafcc9ec2fe95dc
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-03 23:07:32 -0700

    Add query files

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,7 @@
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
